b9cd407677f2d63fc98f4ab64c25d93767bf1169
Benjamin Morgan <benmorgan2@gmail.com>
2019-12-30 11:11:25 +0000

    Documentation for 2x2x2 supercell generation

--- 2x2x2_configurations/2x2x2_configs.py
+++ 2x2x2_configurations/2x2x2_configs.py
@@ -9,6 +9,7 @@ from pymatgen.io.vasp import Poscar
 a = 3.798 # lattice parameter
 poscar_dir = 'poscars'
 
+# construct a ReO3 conventional unit cell as a pymatgen structure
 coords = np.array( [ [ 0.0, 0.0, 0.0 ],
                      [ 0.5, 0.0, 0.0 ],
                      [ 0.0, 0.5, 0.0 ],
@@ -17,11 +18,12 @@ atom_list = [ 'Ti', 'X', 'X', 'X' ]
 lattice = Lattice.from_parameters( a=a, b=a, c=a, alpha=90, beta=90, gamma=90 )
 unit_cell = Structure( lattice, atom_list, coords )
 
+# generate all symmetry-inequivalent 2x2x2 TiOF2 supercells.
 parent_structure = unit_cell * [ 2, 2, 2 ]
 unique_structures = unique_structure_substitutions( parent_structure, 'X', { 'F':16, 'O':8 } )
-
 print( "Found {} unique structures".format( len( unique_structures ) ) )
 
+# save each supercell structure to the poscars directory as a VASP POSCAR.
 if not os.path.exists( poscar_dir ):
     os.makedirs( poscar_dir )
 
