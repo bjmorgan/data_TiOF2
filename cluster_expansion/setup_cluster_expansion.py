24bcb04b9ab85f4fff837315ef8fa015556dadb1
Benjamin Morgan <benmorgan2@gmail.com>
2019-12-30 18:21:08 +0000

    Fixed file paths

--- cluster_expansion/setup_cluster_expansion.py
+++ cluster_expansion/setup_cluster_expansion.py
@@ -5,9 +5,9 @@ from get_structures import copy_poscar_as_str_out
 import numpy as np
 import re
 
-calcs = import_calculations_from_file( '../vasp_calculations/k-points_4x4x4/TiOF2_2x2x2_sample_data.yaml' )
+calcs = import_calculations_from_file( '../2x2x2_vasp_calculations/TiOF2_2x2x2_sample_data.yaml' )
 supercell = np.array( [ 2, 2, 2 ] )
-config_dir = '../configurations/poscars/'
+config_dir = '../2x2x2_configurations/poscars/'
 
 for c in calcs.values():
     nid = re.search( 'config (\d{4})', c.title )[1]
